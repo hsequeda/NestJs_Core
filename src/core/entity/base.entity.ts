cecc08ed42682fb165812456d50fa7ee1a2dbdb2
stdevHsequeda <henrysg88@gmail.com>
2020-11-04 09:48:35 -0500

    Added default values to AppBaseEntity

--- src/core/entity/base.entity.ts
+++ src/core/entity/base.entity.ts
@@ -1,9 +1,20 @@
 import { ObjectType, Field, ID } from '@nestjs/graphql';
-import { PrimaryGeneratedColumn, BaseEntity } from 'typeorm';
+import {
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @ObjectType({ isAbstract: true })
 export class AppBaseEntity extends BaseEntity {
   @Field(() => ID)
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: number;
+  @Field(() => Date, { description: 'Object creation date' })
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+  createdAt: Date;
+  @Field(() => Date, { description: 'Object update date' })
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
+  updatedAt: Date;
 }
