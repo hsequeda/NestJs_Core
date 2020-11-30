c0f02ff357d119d72bb384ad059398a7c0cacca8
stdevHsequeda <henrysg88@gmail.com>
2020-11-30 18:20:07 -0500

    feat(Company) Add company Application.
    Add company application layer with CreateCompanyUseCase and
    ICreateCompanyDTO.

---
+++ src/shared/modules/company/application/dtos/create-company.dto.ts
@@ -0,0 +1,4 @@
+export interface ICreateCompanyDto {
+  name: string;
+  code: string;
+}
---
+++ src/shared/modules/company/application/useCases/create-company.use-case.ts
@@ -0,0 +1,65 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { ICompanyRepository } from '../../domain/interfaces/IRepository';
+import { ICreateCompanyDto } from '../dtos/create-company.dto';
+import { Company } from '../../domain/entities/company.entity';
+import { Name } from '../../domain/value-objects/name.value-object';
+import { Code } from '../../domain/value-objects/code.value-object';
+import { Result, left, right, Either } from 'src/shared/core/Result';
+import { AppError } from 'src/shared/core/errors/AppError';
+import { IUseCase } from 'src/shared/core/interfaces/IUseCase';
+import { CompanyErrors } from '../../domain/errors/company.error';
+
+type Response = Either<
+  | CompanyErrors.CodeExistError
+  | CompanyErrors.NameExistError
+  | AppError.ValidationError
+  | AppError.UnexpectedError,
+  Result<Company>
+>;
+
+@Injectable()
+export class CreateCompanyUseCase
+  implements IUseCase<ICreateCompanyDto, Promise<Response>> {
+  constructor(
+    @Inject() private readonly _companyRepository: ICompanyRepository,
+  ) {}
+
+  async execute(request: ICreateCompanyDto): Promise<Response> {
+    const nameOrErr: Result<Name> = Name.create({
+      value: request.name,
+    });
+    const codeOrErr: Result<Code> = Code.create({
+      value: request.code,
+    });
+    const dtoResult = Result.combine([nameOrErr, codeOrErr]);
+    if (dtoResult.isFailure) {
+      return left(Result.fail(dtoResult.errorValue()));
+    }
+    const companyOrErr: Result<Company> = Company.create({
+      name: nameOrErr.getValue(),
+      code: codeOrErr.getValue(),
+    });
+    if (companyOrErr.isFailure) {
+      return left(Result.fail(companyOrErr.errorValue()));
+    }
+    const company: Company = companyOrErr.getValue();
+    try {
+      const codeExist: boolean = await this._companyRepository.exist({
+        code: { is: company.code.value },
+      });
+      if (!codeExist) {
+        return left(new CompanyErrors.CodeExistError(company.code.value));
+      }
+      const nameExist: boolean = await this._companyRepository.exist({
+        name: { is: company.name.value },
+      });
+      if (!nameExist) {
+        return left(new CompanyErrors.NameExistError(company.name.value));
+      }
+      await this._companyRepository.create(company);
+      return right(Result.ok(company));
+    } catch (err) {
+      return left(new AppError.UnexpectedError(err));
+    }
+  }
+}
