9326b726e2249d3034ebf54df65164b3fa00d30e
stdevHsequeda <henrysg88@gmail.com>
2020-12-02 18:34:34 -0500

    Feat(test) Configure jest.
    Added jest config in jest.config.cjs and removed the test/ folder.

---
+++ jest.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+    verbose: true,
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest'
+    },
+    testRegex: 'src/.*\\.spec\\.ts(x?)$',
+    moduleFileExtensions: [
+        'ts',
+        'tsx',
+        'js'
+    ],
+    moduleNameMapper: {
+        'src/(.*)': '<rootDir>/src/$1',
+    },
+    collectCoverage: false,
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
+        'src/.*\\.spec\\.ts(x?)$'
+    ],
+    coveragePathIgnorePatterns: [
+        '^.+\\.d\\.ts$',
+        'src/full\\.ts$'
+    ]
+};
