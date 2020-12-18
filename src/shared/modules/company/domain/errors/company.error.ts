099d3e6d833ebac82c4679cbde9601a158e30af0
stdevHsequeda <henrysg88@gmail.com>
2020-12-18 12:22:03 -0500

    refactor(CompanyErrors) Implement IResult in all company errors

--- src/shared/modules/company/domain/errors/company.error.ts
+++ src/shared/modules/company/domain/errors/company.error.ts
@@ -1,23 +1,27 @@
-import { IGuardResult } from 'src/shared/core/interfaces/IGuardResult';
+import { UseCaseError } from 'src/shared/core/errors/UseCaseError';
+import { Result } from 'src/shared/core/Result';
 
 export namespace CompanyErrors {
-  export class CodeExistError implements IGuardResult {
-    succeeded: boolean;
-    message: string;
-
-    constructor(name: string) {
-      this.succeeded = false;
-      this.message = `User with name ${name} already exist`;
+  export class CompanyHasBeenDeleted extends Result<UseCaseError> {
+    constructor(deletedAt: Date) {
+      super(false, { message: `Company has been deleted in ${deletedAt}` });
     }
   }
 
-  export class NameExistError implements IGuardResult {
-    succeeded: boolean;
-    message: string;
+  export class CompanyDoesntExist extends Result<UseCaseError> {
+    constructor(id: string) {
+      super(false, { message: `Doesn't exist company with id ${id}` });
+    }
+  }
+  export class CodeExistError extends Result<UseCaseError> {
+    constructor(name: string) {
+      super(false, { message: `User with name '${name}' already exist` });
+    }
+  }
 
+  export class NameExistError extends Result<UseCaseError> {
     constructor(code: string) {
-      this.succeeded = false;
-      this.message = `User with code ${code} already exist`;
+      super(false, { message: `User with code '${code}' already exist` });
     }
   }
 }
