6612892b054e6afb779e491a2fc46a5241e133eb
stdevHsequeda <henrysg88@gmail.com>
2020-12-18 12:12:45 -0500

    refactor(CompanyDomainEntity) Refactor company domain entity
    Implemented AggegateDomainEntity<CompanyProps>. Changed active: boolean field to
    deletedAt?: Date. Add version: Version field. Changed implementation of
    changeName, changeCode and create methods. Add new static method to
    create a new instance of company entity with a new id no persisted.
    Added markHasDeleted method. Added event to new(), markToDeleted(),
    changeName() and changeCode() methods.

--- src/shared/modules/company/domain/entities/company.entity.ts
+++ src/shared/modules/company/domain/entities/company.entity.ts
@@ -1,4 +1,3 @@
-import { DomainEntity } from 'src/shared/domain/entity.abstract';
 import { UniqueEntityID } from 'src/shared/domain/UniqueEntityID';
 import { IGuardArgument } from 'src/shared/core/interfaces/IGuardArgument';
 import { Guard } from 'src/shared/core/Guard';
@@ -6,19 +5,22 @@ import { Result } from 'src/shared/core/Result';
 import { CompanyCode } from '../value-objects/code.value-object';
 import { CompanyName } from '../value-objects/name.value-object';
 import { isNil } from 'lodash';
+import { Version } from 'src/shared/domain/version.value-object';
+import { AggregateDomainEntity } from 'src/shared/domain/aggregate-entity.abstract';
+import { CreatedCompanyEvent } from '../events/create-company.event';
+import { UpdatedCompanyEvent } from '../events/updated-company.event';
+import { DeletedCompanyEvent } from '../events/deleted-company.event';
 
 interface CompanyProps {
   code: CompanyCode;
   name: CompanyName;
   createdAt?: Date;
   updatedAt?: Date;
-  active?: boolean;
+  deletedAt?: Date;
+  version?: Version;
 }
-export class Company extends DomainEntity<CompanyProps> {
-  get id(): string {
-    return this._id.toString();
-  }
 
+export class Company extends AggregateDomainEntity<CompanyProps> {
   get code(): CompanyCode {
     return this.props.code;
   }
@@ -36,22 +38,70 @@ export class Company extends DomainEntity<CompanyProps> {
   }
 
   get isActive(): boolean {
-    return this.props.active;
+    return this.props.deletedAt === undefined;
   }
 
-  changeName(newName: CompanyName): void {
+  get version(): Version {
+    return this.props.version;
+  }
+
+  changeName(newName: CompanyName): Result<void> {
+    if (!this.isActive) {
+      return Result.fail(new Error('This Company has been deleted')); // TODO
+    }
     this.props.name = newName;
+    this.increaseVersion();
     this.props.updatedAt = new Date();
+    this.apply(
+      new UpdatedCompanyEvent(this._id.toString(), this.version.value),
+    );
+    return Result.ok();
   }
 
-  changeCode(newCode: CompanyCode): void {
+  changeCode(newCode: CompanyCode): Result<void> {
+    if (!this.isActive) {
+      return Result.fail(new Error('This Company has been deleted')); // TODO
+    }
     this.props.code = newCode;
+    this.increaseVersion();
     this.props.updatedAt = new Date();
+    this.apply(
+      new UpdatedCompanyEvent(this._id.toString(), this.version.value),
+    );
+    return Result.ok();
+  }
+
+  markHasDeleted(): Result<void> {
+    if (this.isActive) {
+      return Result.fail(new Error('This Company has been deleted')); // TODO
+    }
+
+    this.props.updatedAt = new Date();
+    this.props.deletedAt = new Date();
+    this.apply(
+      new DeletedCompanyEvent(this._id.toString(), this.version.value),
+    );
+    return Result.ok();
+  }
+
+  private increaseVersion(): void {
+    this.props.version = Version.create({
+      value: this.version.value + 1,
+    }).getValue();
+  }
+
+  public static new(props: CompanyProps): Result<Company> {
+    const id = new UniqueEntityID();
+    const companyOrErr: Result<Company> = this.create(props, id);
+    if (companyOrErr.isFailure) return Result.fail(companyOrErr.errorValue());
+    const company: Company = companyOrErr.getValue();
+    company.apply(new CreatedCompanyEvent(id.toString()));
+    return Result.ok(company);
   }
 
   public static create(
     props: CompanyProps,
-    id?: UniqueEntityID,
+    id: UniqueEntityID,
   ): Result<Company> {
     const args: IGuardArgument[] = [
       { argument: props.code, argumentPath: 'code' },
@@ -63,7 +113,8 @@ export class Company extends DomainEntity<CompanyProps> {
     }
     if (isNil(props.createdAt)) props.createdAt = new Date();
     if (isNil(props.updatedAt)) props.updatedAt = new Date();
-    if (isNil(props.active)) props.active = true;
+    if (isNil(props.version))
+      props.version = Version.create({ value: 1 }).getValue();
     return Result.ok(new Company(props, id));
   }
 }
