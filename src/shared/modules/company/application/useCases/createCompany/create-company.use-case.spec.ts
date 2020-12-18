6257f138c780b315170ca9afff0e587919d6e6fb
stdevHsequeda <henrysg88@gmail.com>
2020-12-18 12:09:04 -0500

    test(CreateCompany) Add create company use case test

---
+++ src/shared/modules/company/application/useCases/createCompany/create-company.use-case.spec.ts
@@ -0,0 +1,229 @@
+import { Company } from '../../../domain/entities/company.entity';
+import { ICompanyRepository } from '../../../domain/interfaces/IRepository';
+import {
+  PaginatedFindResult,
+  defaultPaginatedFindResult,
+} from 'src/shared/core/PaginatedFindResult';
+import { Test, TestingModule } from '@nestjs/testing';
+import { CreateCompanyUseCase } from './create-company.use-case';
+import { CqrsModule } from '@nestjs/cqrs';
+import { MockType } from 'src/shared/core/interfaces/MockType';
+import { CompanyErrors } from '../../../domain/errors/company.error';
+import { CompanyName } from '../../../domain/value-objects/name.value-object';
+import { CompanyCode } from '../../../domain/value-objects/code.value-object';
+import { Version } from 'src/shared/domain/version.value-object';
+import { AppError } from 'src/shared/core/errors/AppError';
+
+export const companyRepositoryMock: () => MockType<
+  ICompanyRepository
+> = jest.fn(() => ({
+  existCompanyWithId: jest.fn(async () => false),
+  existCompanyWithName: jest.fn(async () => false),
+  existCompanyWithCode: jest.fn(async () => false),
+  create: jest.fn(async (company: Company) => company),
+  update: jest.fn(async () => {}),
+  delete: jest.fn(async () => {}),
+  paginatedFindCompany: jest.fn(async () => {
+    return defaultPaginatedFindResult as PaginatedFindResult<Company>;
+  }),
+  findOneById: jest.fn(async () => {
+    return {} as Company;
+  }),
+  findAllCompanies: jest.fn(() => {
+    return [{} as Company];
+  }),
+}));
+
+describe('Testing Create-Company Use-Case', () => {
+  let createCompanyUseCase: CreateCompanyUseCase;
+  let companyRepoMock: MockType<ICompanyRepository>;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [CqrsModule],
+      providers: [
+        CreateCompanyUseCase,
+        {
+          provide: 'ICompanyRepository',
+          useFactory: companyRepositoryMock,
+        },
+      ],
+    }).compile();
+    createCompanyUseCase = module.get<CreateCompanyUseCase>(
+      CreateCompanyUseCase,
+    );
+    companyRepoMock = module.get('ICompanyRepository');
+  });
+
+  it('Valid case', async () => {
+    let persistedCompany: Company;
+    companyRepoMock.existCompanyWithCode.mockReturnValue(false);
+    companyRepoMock.existCompanyWithName.mockReturnValue(false);
+    companyRepoMock.create.mockImplementation((company: Company) => {
+      persistedCompany = company;
+      return {};
+    });
+    const resp = await createCompanyUseCase.execute({
+      code: 'LOR',
+      name: 'Lorem ipsum...',
+    });
+
+    expect(resp.isRight()).toBeTruthy();
+    expect(
+      persistedCompany.name.equals(
+        CompanyName.create({ value: 'Lorem ipsum...' }).getValue(),
+      ),
+    ).toBeTruthy();
+    expect(
+      persistedCompany.code.equals(
+        CompanyCode.create({ value: 'LOR' }).getValue(),
+      ),
+    ).toBeTruthy();
+    expect(
+      persistedCompany.version.equals(Version.create({ value: 1 }).getValue()),
+    ).toBeTruthy();
+    expect(persistedCompany.isActive).toBeTruthy();
+    expect(persistedCompany.createdAt.getTime()).toBeLessThan(Date.now());
+    expect(persistedCompany.createdAt.getTime()).toBeLessThan(Date.now());
+  });
+
+  it('Error: name should have at least 1 chars', async () => {
+    companyRepoMock.existCompanyWithCode.mockReturnValue(false);
+    companyRepoMock.existCompanyWithName.mockReturnValue(false);
+    const resp = await createCompanyUseCase.execute({
+      code: 'LOR',
+      name: '',
+    });
+
+    expect(resp.isLeft()).toBeTruthy();
+    expect(resp.value instanceof AppError.ValidationError).toBeTruthy();
+    expect(resp.value.errorValue().message).toEqual(
+      'name should has at least 1 chars',
+    );
+  });
+
+  it('Error: name should be lower than 20 chars', async () => {
+    companyRepoMock.existCompanyWithCode.mockReturnValue(false);
+    companyRepoMock.existCompanyWithName.mockReturnValue(false);
+    const resp = await createCompanyUseCase.execute({
+      code: 'LOR',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    });
+
+    expect(resp.isLeft()).toBeTruthy();
+    expect(resp.value instanceof AppError.ValidationError).toBeTruthy();
+    expect(resp.value.errorValue().message).toEqual(
+      'name should be lower than 20 chars',
+    );
+  });
+
+  it('Error: code should have at least 1 chars', async () => {
+    companyRepoMock.existCompanyWithCode.mockReturnValue(false);
+    companyRepoMock.existCompanyWithName.mockReturnValue(false);
+    const resp = await createCompanyUseCase.execute({
+      code: '',
+      name: 'Lorem ipsum...',
+    });
+
+    expect(resp.isLeft()).toBeTruthy();
+    expect(resp.value instanceof AppError.ValidationError).toBeTruthy();
+    expect(resp.value.errorValue().message).toEqual(
+      'code should has at least 1 chars',
+    );
+  });
+
+  it('Error: name should be lower than 20 chars', async () => {
+    companyRepoMock.existCompanyWithCode.mockReturnValue(false);
+    companyRepoMock.existCompanyWithName.mockReturnValue(false);
+    const resp = await createCompanyUseCase.execute({
+      code: 'LOR',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    });
+
+    expect(resp.isLeft()).toBeTruthy();
+    expect(resp.value instanceof AppError.ValidationError).toBeTruthy();
+    expect(resp.value.errorValue().message).toEqual(
+      'name should be lower than 20 chars',
+    );
+  });
+
+  it('Error: code exist', async () => {
+    companyRepoMock.existCompanyWithCode.mockReturnValue(true);
+    companyRepoMock.existCompanyWithName.mockReturnValue(false);
+    const resp = await createCompanyUseCase.execute({
+      code: 'LOR',
+      name: 'Lorem ipsum...',
+    });
+
+    expect(resp.isLeft()).toBeTruthy();
+    expect(resp.value instanceof CompanyErrors.CodeExistError).toBeTruthy();
+    expect(resp.value.errorValue()).toEqual(
+      new CompanyErrors.CodeExistError('LOR').errorValue(),
+    );
+  });
+
+  it('Error: name exist', async () => {
+    companyRepoMock.existCompanyWithCode.mockReturnValue(false);
+    companyRepoMock.existCompanyWithName.mockReturnValue(true);
+    const resp = await createCompanyUseCase.execute({
+      code: 'LOR',
+      name: 'Lorem ipsum...',
+    });
+    expect(resp.isLeft()).toBeTruthy();
+    expect(resp.value instanceof CompanyErrors.NameExistError).toBeTruthy();
+    expect(resp.value.errorValue()).toEqual(
+      new CompanyErrors.NameExistError('Lorem ipsum...').errorValue(),
+    );
+  });
+
+  it('Error: code undefined', async () => {
+    companyRepoMock.existCompanyWithCode.mockReturnValue(false);
+    companyRepoMock.existCompanyWithName.mockReturnValue(true);
+    const resp = await createCompanyUseCase.execute({
+      code: undefined,
+      name: 'Lorem ipsum...',
+    });
+
+    expect(resp.isLeft()).toBeTruthy();
+    expect(resp.value instanceof AppError.ValidationError).toBeTruthy();
+    expect(resp.value.errorValue().message).toEqual('code should be defined');
+  });
+
+  it('Error: name undefined', async () => {
+    companyRepoMock.existCompanyWithCode.mockReturnValue(false);
+    companyRepoMock.existCompanyWithName.mockReturnValue(true);
+    const resp = await createCompanyUseCase.execute({
+      code: 'LOR',
+      name: undefined,
+    });
+
+    expect(resp.isLeft()).toBeTruthy();
+    expect(resp.value instanceof AppError.ValidationError).toBeTruthy();
+    expect(resp.value.errorValue().message).toEqual('name should be defined');
+  });
+
+  it('Error: code null', async () => {
+    companyRepoMock.existCompanyWithCode.mockReturnValue(false);
+    companyRepoMock.existCompanyWithName.mockReturnValue(true);
+    const resp = await createCompanyUseCase.execute({
+      code: undefined,
+      name: 'Lorem ipsum...',
+    });
+
+    expect(resp.isLeft()).toBeTruthy();
+    expect(resp.value instanceof AppError.ValidationError).toBeTruthy();
+    expect(resp.value.errorValue().message).toEqual('code should be defined');
+  });
+
+  it('Error: name null', async () => {
+    companyRepoMock.existCompanyWithCode.mockReturnValue(false);
+    companyRepoMock.existCompanyWithName.mockReturnValue(true);
+    const resp = await createCompanyUseCase.execute({
+      code: 'LOR',
+      name: undefined,
+    });
+
+    expect(resp.isLeft()).toBeTruthy();
+    expect(resp.value instanceof AppError.ValidationError).toBeTruthy();
+    expect(resp.value.errorValue().message).toEqual('name should be defined');
+  });
+});
