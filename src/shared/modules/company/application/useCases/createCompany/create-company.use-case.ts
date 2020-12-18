10ddde5a2fe08087e43a14cdcdf87f44efc7c9b5
stdevHsequeda <henrysg88@gmail.com>
2020-12-18 12:04:30 -0500

    refactor(createCompany) Refactor create company use case
    Moved created-company.use-case.ts from
    src/shared/modules/company/application/useCases/create-company.use-case.ts
    to src/shared/modules/company/application/useCases/createCompany/create-company.use-case.ts.
    Used eventual consistence in create-company use case.

---
+++ src/shared/modules/company/application/useCases/createCompany/create-company.use-case.ts
@@ -0,0 +1,77 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { Either, Result, left, right } from 'src/shared/core/Result';
+import { CompanyErrors } from '../../../domain/errors/company.error';
+import { Company } from '../../../domain/entities/company.entity';
+import { IUseCase } from 'src/shared/core/interfaces/IUseCase';
+import { CreateCompanyDto } from '../../dtos/create-company.dto';
+import { ICompanyRepository } from '../../../domain/interfaces/IRepository';
+import { CompanyName } from '../../../domain/value-objects/name.value-object';
+import { CompanyCode } from '../../../domain/value-objects/code.value-object';
+import { AppError } from 'src/shared/core/errors/AppError';
+import { EventPublisher } from '@nestjs/cqrs';
+import { Version } from 'src/shared/domain/version.value-object';
+
+export type CreateCompanyResponse = Either<
+  | CompanyErrors.CodeExistError
+  | CompanyErrors.NameExistError
+  | AppError.ValidationError<CompanyName | CompanyCode | Version>
+  | AppError.UnexpectedError,
+  Result<void>
+>;
+
+@Injectable()
+export class CreateCompanyUseCase
+  implements IUseCase<CreateCompanyDto, Promise<CreateCompanyResponse>> {
+  constructor(
+    @Inject('ICompanyRepository')
+    private readonly _companyRepository: ICompanyRepository,
+    private readonly _publisher: EventPublisher,
+  ) {}
+
+  async execute(request: CreateCompanyDto): Promise<CreateCompanyResponse> {
+    const nameOrErr: Result<CompanyName> = CompanyName.create({
+      value: request.name,
+    });
+    const codeOrErr: Result<CompanyCode> = CompanyCode.create({
+      value: request.code,
+    });
+    const dtoResult = Result.combine([nameOrErr, codeOrErr]);
+    if (dtoResult.isFailure) {
+      return left(dtoResult);
+    }
+    const companyOrErr: Result<Company> = Company.new({
+      name: nameOrErr.getValue(),
+      code: codeOrErr.getValue(),
+    });
+
+    if (companyOrErr.isFailure) {
+      return left(companyOrErr);
+    }
+
+    const company: Company = this._publisher.mergeObjectContext(
+      companyOrErr.getValue(),
+    );
+
+    const codeExist: boolean = await this._companyRepository.existCompanyWithCode(
+      company.code.value,
+    );
+    if (codeExist) {
+      return left(new CompanyErrors.CodeExistError(company.code.value));
+    }
+
+    const nameExist: boolean = await this._companyRepository.existCompanyWithName(
+      company.name.value,
+    );
+    if (nameExist) {
+      return left(new CompanyErrors.NameExistError(company.name.value));
+    }
+
+    try {
+      await this._companyRepository.create(company);
+      company.commit();
+      return right(Result.ok());
+    } catch (err) {
+      return left(new AppError.UnexpectedError(err));
+    }
+  }
+}
