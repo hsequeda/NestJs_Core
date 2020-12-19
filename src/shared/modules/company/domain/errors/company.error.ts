98d29962e3554a13052883b7f2b36d679fc15c93
stdevHsequeda <henrysg88@gmail.com>
2020-12-18 23:07:22 -0500

    refactor(CompanyErrors) Add logger to company errors

--- src/shared/modules/company/domain/errors/company.error.ts
+++ src/shared/modules/company/domain/errors/company.error.ts
@@ -1,27 +1,36 @@
 import { UseCaseError } from 'src/shared/core/errors/UseCaseError';
 import { Result } from 'src/shared/core/Result';
+import { Logger } from '@nestjs/common';
 
 export namespace CompanyErrors {
   export class CompanyHasBeenDeleted extends Result<UseCaseError> {
-    constructor(deletedAt: Date) {
-      super(false, { message: `Company has been deleted in ${deletedAt}` });
+    constructor() {
+      super(false, { message: 'Company has been deleted' });
+      Logger.log('Error: Company has been deleted', 'CompanyError');
+      Logger.error(this.errorValue().message);
     }
   }
 
   export class CompanyDoesntExist extends Result<UseCaseError> {
     constructor(id: string) {
       super(false, { message: `Doesn't exist company with id ${id}` });
+      Logger.log(`Error: Company doesn't exist`, 'CompanyError');
+      Logger.error(this.errorValue().message);
     }
   }
   export class CodeExistError extends Result<UseCaseError> {
     constructor(name: string) {
       super(false, { message: `User with name '${name}' already exist` });
+      Logger.log('Error: Code exist error', 'CompanyError');
+      Logger.error(this.errorValue().message);
     }
   }
 
   export class NameExistError extends Result<UseCaseError> {
     constructor(code: string) {
       super(false, { message: `User with code '${code}' already exist` });
+      Logger.log('Error: Name exist error', 'CompanyError');
+      Logger.error(this.errorValue().message);
     }
   }
 }
