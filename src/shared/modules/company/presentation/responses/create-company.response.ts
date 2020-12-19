9444777970ab3bc8c1b14c26aa9e3959ad83bde8
stdevHsequeda <henrysg88@gmail.com>
2020-12-18 23:09:42 -0500

    feat(CompanyPresentationResponse) Add Validation and unexpected error to CreateCompanyResonse

--- src/shared/modules/company/presentation/responses/create-company.response.ts
+++ src/shared/modules/company/presentation/responses/create-company.response.ts
@@ -1,8 +1,16 @@
 import { createUnionType } from '@nestjs/graphql';
 import { CodeExistError } from './code-exist.error';
 import { NameExistError } from './name-exist.error';
+import { ValidationError } from 'src/shared/core/presentation/responses/validation.error';
+import { UnexpectedError } from 'src/shared/core/presentation/responses/unexpected.error';
 
 export const CreateCompanyResponse = createUnionType({
   name: 'CreateCompanyResponse',
-  types: () => [Boolean, NameExistError, CodeExistError],
+  types: () => [
+    Boolean,
+    NameExistError,
+    CodeExistError,
+    ValidationError,
+    UnexpectedError,
+  ],
 });
