9429be734f0ee340d22831dc99c095f5ea38c0fe
stdevHsequeda <henrysg88@gmail.com>
2020-12-19 19:33:04 -0500

    refactor(GraphqlModule) Make graphql module global

--- src/shared/modules/graphql/graphql.module.ts
+++ src/shared/modules/graphql/graphql.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { graphqlProvider } from './graphql.provider';
 import { gqlPubSubProvider } from './gql-pubsub.provider';
 
+@Global()
 @Module({
   imports: [graphqlProvider],
   providers: [gqlPubSubProvider],
