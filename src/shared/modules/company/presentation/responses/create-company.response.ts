57fb7fda4d789fd147a38c63d6d34c1b452f6905
stdevHsequeda <henrysg88@gmail.com>
2020-12-21 18:41:09 -0500

    refactor(CreateCompanyResponse) Rename from Response to SuccessResponse the posible type in CreateCompanyResponse

--- src/shared/modules/company/presentation/responses/create-company.response.ts
+++ src/shared/modules/company/presentation/responses/create-company.response.ts
@@ -3,12 +3,12 @@ import { CodeExistError } from './code-exist.error';
 import { NameExistError } from './name-exist.error';
 import { ValidationError } from 'src/shared/core/presentation/responses/validation.error';
 import { UnexpectedError } from 'src/shared/core/presentation/responses/unexpected.error';
-import { Response } from 'src/shared/core/presentation/responses/success.response';
+import { SuccessResponse } from 'src/shared/core/presentation/responses/success.response';
 
 export const CreateCompanyResponse = createUnionType({
   name: 'CreateCompanyResponse',
   types: () => [
-    Response,
+    SuccessResponse,
     NameExistError,
     CodeExistError,
     ValidationError,
